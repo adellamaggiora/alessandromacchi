1fe50184b1e2d8922f33751f07b669a1b706a042
andrea <adm91@email.com>
2022-10-20 23:39:30 +0200

    toolbar ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
+  menuOpened: boolean;
+
+  openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    setTimeout(() => {
+      this.menuOpened = true;  
+    }, 150);
+    
   }
+  
+  closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    this.menuOpened = false;
+  } 
 
 }
